6e677ef1dd23918f608b698c6ca3467d4230821d
vitinnnzl <108942584+vitinnnzl@users.noreply.github.com>
2024-10-04 09:46:26 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,11 +1,19 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarDadosGlobais(){
-    const res = await fetch(url)
+async function  visualizarDadosGlobais(){
+    const res = await fetch(url);
+    const secao = document.querySelector('.graficos-container')
     const dados = await res.json();
+    const totalPessoasMundo = dados.total_pessoas_mundo
+    const totalPessoasConectadas = dados.total_pessoas_conectadas
+    const tempoMedio = dados.tempo_medio
     console.log(dados);
     const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-texto')
-
+    paragrafo.classList.add('graficos-texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem
+    <span> ${totalPessoasMundo} de habitantes e destes </span> ${totalPessoasConectadas} </span> estão conectadas a alguma rede social e ficam em média <span> ${tempoMedio} </span>conectados.`
+    secao.appendChild(paragrafo)
 }
+
+
 visualizarDadosGlobais();
